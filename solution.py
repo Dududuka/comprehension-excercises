c5918c030f203260beec8270fc419fadf9685b60
Larysa-Plostak <izumrud@gmail.com>
2022-06-27 10:23:04 -0700

    solved

---
+++ solution.py
@@ -0,0 +1,12 @@
+for i in range (100):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+
+fizzbuzz = ["FizzBuzz" if i % 3 == 0 and i % 5 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else i for i in range(100)]
+print(fizzbuzz)
